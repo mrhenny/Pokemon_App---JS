69d27de23aa760484ca79fd1d49799c7f4eaf1bc
codewithdivu <mavadiyadivyesh56@gmail.com>
2023-03-18 18:33:56 +0530

    few changes in pokemon.js

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,74 +1,3 @@
-// function fetchdata(){
-//     fetch('https://pokeapi.co/api/v2/pokemon?limit=1')
-//     .then(res => res.json())
-//     .then(function(allpokemon){
-//         allpokemon.results.forEach(function(pokemon){
-//             fetchPokemonData(pokemon);
-//         });
-//     })
-
-//     function fetchPokemonData(pokemon){
-//         let url = pokemon.url
-//           fetch(url)
-//           .then(response => response.json())
-//           .then(function(pokeData){
-//           console.log(pokeData)
-//           })
-//         }
-// }
-
-// fetchdata();
-// async function pokeinfo(){
-
-//     const url = `https://pokeapi.co/api/v2/pokemon?limit=11`
-//     const result = await fetch(url).then((res)=>res.json())
-
-//     console.log(result.results[10].url);
-
-//     // justry.innerHtml
-// }
-
-// pokeinfo();
-
-// let arr = [];
-// let type = "grass";
-
-// function fetchPokemonData(pokemon) {
-//   let url = pokemon.url;
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then(function (pokeData) {
-//       // console.log("lodu", pokeData);
-//       arr.push(pokeData);
-//       // console.log("aaru", arr);
-//     });
-// }
-
-// async function pokeinfo() {
-//   const url = `https://pokeapi.co/api/v2/pokemon`;
-//   const response = await fetch(url)
-//     .then((res) => res.json())
-//     .then(function (allpokemon) {
-//       allpokemon.results.forEach(function (pokemon) {
-//         fetchPokemonData(pokemon);
-//       });
-//     });
-// }
-// pokeinfo();
-
-// let newPoko = [];
-
-// let newArr = arr.forEach((item) => {
-//   item.types.forEach((it) => {
-//     if (it.type.name === type) {
-//       newPoko.push(item);
-//     }
-//   });
-// });
-
-// console.log("newPoko", newPoko);
-// console.log("arr", arr);
-
 var arr = [];
 var newArr = [];
 
@@ -95,7 +24,6 @@ async function pokeinfo(num) {
     .then((res) => res.json())
     .then((response) => arr.push(response));
 }
-// console.log("arrr", arr);
 
 console.log("newPoko", newArr);
 
