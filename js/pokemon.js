31d18371ced8c71a107f1b1d2ffbe53d0b9f38fb
codewithdivu <mavadiyadivyesh56@gmail.com>
2023-03-18 18:30:03 +0530

    added filter of pokemons

--- js/pokemon.js
+++ js/pokemon.js
@@ -40,7 +40,7 @@
 //     .then(function (pokeData) {
 //       // console.log("lodu", pokeData);
 //       arr.push(pokeData);
-//       console.log("aaru", arr);
+//       // console.log("aaru", arr);
 //     });
 // }
 
@@ -65,19 +65,59 @@
 //     }
 //   });
 // });
+
+// console.log("newPoko", newPoko);
+// console.log("arr", arr);
+
 var arr = [];
+var newArr = [];
 
 window.onload = () => {
-  for (let i = 1; i <= 20; i++) {
-    console.log("hi", i);
-    pokeinfo(i);
+  arr.forEach((item) => {
+    item.types.forEach((it) => {
+      if (it.type.name === "grass") {
+        newArr.push(item);
+        return;
+      }
+    });
+  });
+
+  if (newArr.length > 0) {
+    mapPokemons();
   }
 };
-
+for (let i = 1; i <= 20; i++) {
+  pokeinfo(i);
+}
 async function pokeinfo(num) {
   const url = `https://pokeapi.co/api/v2/pokemon/${num}`;
   const response = await fetch(url)
     .then((res) => res.json())
     .then((response) => arr.push(response));
 }
-console.log("arrr", arr);
+// console.log("arrr", arr);
+
+console.log("newPoko", newArr);
+
+function mapPokemons() {
+  let main = document.getElementById("main");
+  main.innerHTML = "";
+  newArr.length > 0 &&
+    newArr.map((item) => {
+      main.innerHTML += `
+      <div class="typeofpokemon typeofpokemon1">
+      <div class="logoname">
+        <span class="nameoftype">${item.name}</span>
+        <span>${item.id}</span>
+        <span>${item.weight}</span>
+        <div class="justry"></div>
+      </div>
+      <img
+        class="electricpokemon"
+        src="../images/electricpokemon.png"
+        alt="electricpokemon"
+      />
+    </div> 
+      `;
+    });
+}
